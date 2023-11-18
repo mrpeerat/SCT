4105c4426b7893be9c1648f9c0e3bfe9b0fd77ad
KornWtp <elecwpr@gmail.com>
2023-11-18 12:22:24 +0000

    Updata file

--- sentence_transformers/__init__.py
+++ sentence_transformers/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "2.1.0"
-__MODEL_HUB_ORGANIZATION__ = 'sentence-transformers'
+# __MODEL_HUB_ORGANIZATION__ = 'sentence-transformers'
 from .datasets import SentencesDataset, ParallelSentencesDataset
 from .LoggingHandler import LoggingHandler
 from .SentenceTransformer import SentenceTransformer
