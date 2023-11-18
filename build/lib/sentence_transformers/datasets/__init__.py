691510cf3985e4405e21106b072ca029629ad9aa
KornWtp <elecwpr@gmail.com>
2023-11-18 13:39:39 +0000

    remove sentence-transformers

--- build/lib/sentence_transformers/datasets/__init__.py
+++
@@ -1,6 +0,0 @@
-from .DenoisingAutoEncoderDataset import DenoisingAutoEncoderDataset
-from .NoDuplicatesDataLoader import NoDuplicatesDataLoader
-from .ParallelSentencesDataset import ParallelSentencesDataset
-from .ParallelSentencesDatasetForSEEDER import ParallelSentencesDatasetForSEEDER
-from .SentencesDataset import SentencesDataset
-from .SentenceLabelDataset import SentenceLabelDataset
